28988ab0d6d19da50986cdbd37b90553e974d3df
Alkesh Nikalje <alkeshnikalje@gmail.com>
2024-01-06 14:27:51 +0530

    style accordian

--- src/components/home/AccordianItem.jsx
+++ src/components/home/AccordianItem.jsx
@@ -14,7 +14,7 @@ function AccordianItem({ question, answer, idx, setIndex, isClicked }) {
             viewBox="0 0 36 36"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            class="elj7tfr3 e164gv2o4 default-ltr-cache-l7vm9h e1svuwfo1 text-white"
+            class="elj7tfr3 e164gv2o4 default-ltr-cache-l7vm9h e1svuwfo1 text-white transition-all duration-300"
             data-name="Plus"
             alt=""
           >
@@ -27,26 +27,25 @@ function AccordianItem({ question, answer, idx, setIndex, isClicked }) {
           </svg>
         ) : (
           <svg
-            width="36"
-            height="36"
-            viewBox="0 0 36 36"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            class="elj7tfr3 e164gv2o4 default-ltr-cache-1javmrk e1svuwfo1"
-            data-name="Plus"
-            alt=""
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-10 h-10  text-white transition-all duration-300"
           >
             <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M17 17V3H19V17H33V19H19V33H17V19H3V17H17Z"
-              fill="currentColor"
-            ></path>
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
           </svg>
         )}
       </div>
       {isClicked && (
-        <p className="text-white text-lg p-4 bg-rgb mt-[1.5px]">{answer}</p>
+        <p className="text-white text-lg p-4 bg-rgb mt-[1.5px] transition-all duration-300">
+          {answer}
+        </p>
       )}
     </div>
   );
